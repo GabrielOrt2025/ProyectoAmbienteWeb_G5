af52ec3afeb7831840302e31b6b2d8d1beac582d
GabrielOrt2025 <gortega00053@ufide.ac.cr>
2025-12-02 21:15:36 -0600

    pinga
    11

---
+++ DataBase/DataBase.php
@@ -0,0 +1,20 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$password = "pene123";
+$base_datos = "lavaca_shop";
+
+
+$conexion = mysqli_connect($servidor, $usuario, $password, $base_datos);
+
+
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+
+mysqli_set_charset($conexion, "utf8");
+
+ echo "Conexión exitosa";
+?>
--- js/accesibilidad.js
+++ js/accesibilidad.js
@@ -9,29 +9,31 @@ window.onload = function() {
     };
   });
 
-
+  //Queryselectorall nos ayuda a que se tomen en cuenta todos los botones de editar para activar el script
   document.querySelectorAll('.button.is-warning').forEach(boton => {
     boton.onclick = function() {
-      const card = this.closest('.card');
-      if (card.querySelector('.form-editar')) return;
-
-      const form = document.createElement('div');
-      form.className = 'form-editar';
-      form.innerHTML = `
+    //variable que localiza el card en la que se quiere hacer el cambio
+      const producto = this.closest('.card');
+      if (producto.querySelector('.form-editar')) return;
+        //Se crea un formulario a partir de la variable formulario usando html interno
+      const formulario = document.createElement('div');
+      formulario.className = 'formulario-editar';
+      formulario.innerHTML = `
         <p><b>Editar producto</b></p>
-        <input type="text" placeholder="Nuevo nombre"><br><br>
-        <input type="number" placeholder="Nuevo precio"><br><br>
+        <input type="text" placeholder="Nombre"><br><br>
+        <input type="number" placeholder="Precio"><br><br>
         <input type="file" accept="image/*"><br><br>
         <button class="guardar">Guardar</button>
         <button class="cancelar">Cancelar</button>
       `;
-
-      card.appendChild(form);
-
-      form.querySelector('.cancelar').onclick = () => form.remove();
-      form.querySelector('.guardar').onclick = () => {
-        alert('Simulación: producto editado.');
-        form.remove();
+    //appendchild hace que el formulario aparezca dentro del mismo card
+      card.appendChild(formulario);
+        //si se hace click en el boton cancelar, solo se cierra el formulario con formulario.remove
+      formulario.querySelector('.cancelar').onclick = () => formulario.remove();
+      //Si se hace click en el boton guardar, se actualiza la información nueva
+      formulario.querySelector('.guardar').onclick = () => {
+        alert('producto editado correctamente');
+        formulario.remove();
       };
     };
   });
